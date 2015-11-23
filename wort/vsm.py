289e6053f55ccd7d5bfd2ca3f82dd80c7ca72eb4
tttthomasssss <tttthomasssss@gmail.com>
2015-11-23 17:01:25 +0000

    removed memmap stuff

--- wort/vsm.py
+++ wort/vsm.py
@@ -24,8 +24,7 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 	def __init__(self, window_size, weighting='ppmi', min_frequency=0, lowercase=True, stop_words=None, encoding='utf-8',
 				 max_features=None, preprocessor=None, tokenizer=None, analyzer='word', binary=False, sppmi_shift=1,
 				 token_pattern=r'(?u)\b\w\w+\b', decode_error='strict', strip_accents=None, input='content',
-				 ngram_range=(1, 1), cds=1, svd_dim=None, svd_eig_weighting=1, add_context_vectors=True,
-				 use_memmap=False, memmap_path=None):
+				 ngram_range=(1, 1), cds=1, svd_dim=None, svd_eig_weighting=1, add_context_vectors=True):
 
 		self.window_size = window_size
 		self.weighting = weighting
@@ -42,8 +41,6 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 		self.token_pattern = token_pattern
 		self.decode_error = decode_error
 		self.strip_accents = strip_accents
-		self.use_memmap = use_memmap
-		self.memmap_path = memmap_path
 		self.input = input
 		self.ngram_range = ngram_range
 		self.cds = cds
@@ -153,36 +150,6 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 		if (self.binary):
 			self.M_ = np.minimum(self.M_, 1)
 
-	'''
-	def _construct_cooccurrence_matrix_mmap(self):
-		self.M_ = np.memmap(os.path.join(self.memmap_path, 'M.dat'), dtype=np.uint32, mode='w+', shape=(self.vocab_count_, self.vocab_count_))
-		self.M_[:] = np.zeros((self.vocab_count_, self.vocab_count_))
-
-		for idx in self.index_.keys():
-			occurrences = np.where(self.temp_tokens_store_==idx)[0]
-			for occ_idx in occurrences:
-				cooccurrence_window = np.concatenate((self.temp_tokens_store_[occ_idx - self.window_size:occ_idx], self.temp_tokens_store_[occ_idx + 1:occ_idx + 1 + self.window_size]))
-
-				self.M_[idx, cooccurrence_window] += 1
-
-		self.M_.flush()
-
-	def _construct_cooccurrence_matrix(self, raw_documents):
-		self.M_ = sparse.dok_matrix((self.vocab_count_, self.vocab_count_), dtype=np.uint32)
-
-		#for doc in raw_documents:
-		#	for feature in self.analyser_fn_(doc):
-
-
-		# To mem heavy
-		for idx in self.index_.keys():
-			occurrences = np.where(self.temp_tokens_store_==idx)[0]
-			for occ_idx in occurrences:
-				cooccurrence_window = np.concatenate((self.temp_tokens_store_[occ_idx - self.window_size:occ_idx], self.temp_tokens_store_[occ_idx + 1:occ_idx + 1 + self.window_size]))
-
-				self.M_[idx, cooccurrence_window] += 1
-	'''
-
 	def _apply_weight_option(self, PMI, P_w_c, p_c):
 		if (self.weighting == 'ppmi'):
 			return PMI
@@ -192,30 +159,6 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 			raise NotImplementedError #(P_w_c[idx, row] * (1 / -(np.log(p_c)))) * pmi # TODO
 		elif (self.weighting == 'sppmi'):
 			return PMI - np.log(self.sppmi_shift)
-
-	def _transform_memmap(self):
-		self.T_ = np.memmap(os.path.join(self.memmap_path, '%s.dat' % (self.weighting.upper(),)), dtype=np.uint32, mode='w+', shape=(self.vocab_count_, self.vocab_count_))
-		self.T_[:] = np.zeros((self.vocab_count_, self.vocab_count_), dtype=np.float64)
-
-		# Joint Probability for all co-occurrences, P(w, c) = P(c | w) * P(w) = P(w | c) * P(c)
-		P_w_c = self.M_ / self.M_.sum(axis=1) * self.p_w_.reshape(-1, 1)
-
-		# Perform weighting
-		for idx in self.index_.keys():
-			row = np.where(self.M_[idx, :] > 0)[0]
-
-			# Marginals for context
-			p_c = self.p_w_[row] ** self.cds
-
-			# PMI
-			pmi = np.log(P_w_c[idx, row]) - (np.log(self.p_w_[idx]) + np.log(p_c))
-
-			# Apply PMI variant (e.g. PPMI, SPPMI, PLMI or PNPMI)
-			tpmi = self._apply_weight_option(pmi, self.weighting, p_c, idx, row)
-
-			self.T_[idx, row] = np.maximum(0, tpmi)
-
-		return self.T_
 
 	def _weight_transformation(self):
 		self.T_ = sparse.lil_matrix(self.M_.shape, dtype=np.float64)
@@ -267,12 +210,6 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 
 		self._construct_cooccurrence_matrix(raw_documents)
 
-		# Construct Co-Occurrence Matrix
-		#if (self.use_memmap):
-		#	self._construct_cooccurrence_matrix_mmap()
-		#else:
-		#	self._construct_cooccurrence_matrix(raw_documents)
-
 		# Apply weighting transformation
 		self._weight_transformation()
 
@@ -280,11 +217,6 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 
 	def transform(self, raw_documents):
 		# todo move to a different class or rename?
-		# Apply the weighting transformation
-		#if (self.use_memmap):
-		#	return sparse.csr_matrix(self._transform_memmap())
-		#else:
-		#	return self._transform()
 		if (self.T_ is not None):
 			return self.T_
 		else: # TODO: perform lookup
