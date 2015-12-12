1073115fef34aa4f4a995882bae3a93d340ae6f1
tttthomasssss <th0mas.ko6er@gmail.com>
2015-12-12 12:01:15 +0000

    added movie review reader

--- wort/corpus_readers.py
+++ wort/corpus_readers.py
@@ -2,6 +2,8 @@ __author__ = 'thomas'
 import csv
 import sys
 
+import joblib
+
 csv.field_size_limit(sys.maxsize)
 
 
@@ -15,3 +17,14 @@ class WikipediaReader(object):
 
 			for line in csv_reader:
 				yield line[1]
+
+
+class MovieReviewReader(object):
+	def __init__(self, path):
+		self.path_ = path
+
+	def __iter__(self):
+		unlabelled_docs = joblib.load(self.path_)
+
+		for doc in unlabelled_docs:
+			yield doc.replace('<br />', ' ')
