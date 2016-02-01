5d253a99e40fde7532f63fe14d63c0b664b25917
tttthomasssss <th0mas.ko6er@gmail.com>
2016-02-01 22:18:39 +0000

    try it again and again anda again

--- wort/vsm.py
+++ wort/vsm.py
@@ -183,11 +183,11 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 					data.append(1) # TODO: Apply dynamic context window weighting here!
 
 		logging.info('Creating sparse matrix...')
-		data = np.array(data, dtype=np.uint32, copy=False)
-		rows = np.array(rows, dtype=np.uint32, copy=False)
-		cols = np.array(cols, dtype=np.uint32, copy=False)
+		data = np.array(data, dtype=np.uint64, copy=False)
+		rows = np.array(rows, dtype=np.uint64, copy=False)
+		cols = np.array(cols, dtype=np.uint64, copy=False)
 
-		self.M_ = sparse.coo_matrix((data, (rows, cols)), dtype=np.uint64) # Scipy seems to not handle numeric overflow in a very graceful manner
+		self.M_ = sparse.coo_matrix((data, (rows, cols)), dtype=np.uint64).tocsr() # Scipy seems to not handle numeric overflow in a very graceful manner
 		logging.info('M.shape={}'.format(self.M_.shape))
 
 		# Apply Binarisation
@@ -212,8 +212,8 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 		# Joint Probability for all co-occurrences, P(w, c) = P(c | w) * P(w) = P(w | c) * P(c)
 		# Doing it this way, keeps P_w_c a sparse matrix: http://stackoverflow.com/questions/3247775/how-to-elementwise-multiply-a-scipy-sparse-matrix-by-a-broadcasted-dense-1d-arra
 		P_w = sparse.lil_matrix(self.M_.shape, dtype=np.float64)
-		logging.info('M sum={}'.format(self.M_.sum(axis=1).shape)) # TODO: Shit goes wrong here!!!!!!!!
 		logging.info('M sum max={}'.format(np.amax(self.M_.sum(axis=1))))
+		logging.info('M sum={}'.format(self.M_.sum(axis=1).shape)) # TODO: Shit goes wrong here!!!!!!!!
 		logging.info('M_dtype={}; M_sum_dtype={}'.format(self.M_.dtype, self.M_.sum(axis=1).dtype))
 		logging.info('New p_w_ shape={}; M.sum(axis=1) shape={}'.format(self.p_w_.reshape(-1, 1).shape, self.M_.sum(axis=1).shape)) # TODO: THE ERROR IS IN ONE OF THE TWO CALLS!!!!!!!!
 		P_w.setdiag((self.p_w_.reshape(-1, 1) / self.M_.sum(axis=1)))
