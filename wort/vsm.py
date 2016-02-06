51e25a9d6cb0b2cb41ff5572150c8dd0978b1b92
tttthomasssss <th0mas.ko6er@gmail.com>
2016-02-06 03:58:24 +0000

    fucking density check

--- wort/vsm.py
+++ wort/vsm.py
@@ -324,7 +324,7 @@ class VSMVectorizer(BaseEstimator, VectorizerMixin):
 			else:
 				self.T_ = W
 
-		logging.info('Returning [density={}]...'.format(np.count_nonzero(self.T_) / (self.T_.shape[0] * self.T_.shape[1])))
+		logging.info('Returning [density={}]...'.format(len(self.T_.nonzero()[0]) / (self.T_.shape[0] * self.T_.shape[1])))
 
 		return self.T_
 
