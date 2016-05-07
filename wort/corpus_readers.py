0a3ab49be76854a38f20bbdd8f12926c6f31b398
tttthomasssss <th0mas.ko6er@gmail.com>
2016-05-07 23:07:28 +0100

    cleanup

--- wort/corpus_readers.py
+++ wort/corpus_readers.py
@@ -3,8 +3,6 @@ import csv
 import gzip
 import sys
 
-import joblib
-
 csv.field_size_limit(sys.maxsize)
 
 
@@ -45,24 +43,3 @@ class CSVStreamReader(object):
 
 			for line in csv_reader:
 				yield line[0]
-
-
-class MovieReviewReader(object):
-	def __init__(self, path):
-		self.path_ = path
-
-	def __iter__(self):
-		unlabelled_docs = joblib.load(self.path_)
-
-		for doc in unlabelled_docs:
-			yield doc.replace('<br />', ' ')
-
-
-class FrostReader(object):
-	def __init__(self, path):
-		self.path_ = path
-
-	def __iter__(self):
-		with open(self.path_, 'r') as frost:
-			for line in frost:
-				yield line
