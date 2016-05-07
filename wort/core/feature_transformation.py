9e00246eaa442a8477e4facf4bdf6ef6c1070674
tttthomasssss <th0mas.ko6er@gmail.com>
2016-05-07 18:42:20 +0100

    cleanup

--- wort/core/feature_transformation.py
+++ wort/core/feature_transformation.py
@@ -2,15 +2,15 @@ __author__ = 'thomas'
 import numpy as np
 
 
-def ppmi_transformation(PMI, P_w_c, p_w, p_c):
+def ppmi_transformation(PMI, *_):
 	return PMI
 
 
-def plmi_transformation(PMI, P_w_c, p_w, p_c): # TODO: Needs testing (pizza_small.txt)
+def plmi_transformation(PMI, P_w_c, *_): # TODO: Needs testing (pizza_small.txt)
 	return P_w_c.multiply(PMI)
 
 
-def pnpmi_transformation(PMI, P_w_c, p_w, p_c): #TODO: Needs testing (pizza_small.txt)
+def pnpmi_transformation(PMI, P_w_c, *_): #TODO: Needs testing (pizza_small.txt)
 	# Tricky one, could normalise by -log(P(w)), -log(P(c)) or -log(P(w, c)); choose the latter because it normalises the upper & the lower bound,
 	# and is nicer implementationwise (see Bouma 2009: https://svn.spraakdata.gu.se/repos/gerlof/pub/www/Docs/npmi-pfd.pdf)
 	P_w_c.data = 1 / -np.log(P_w_c.data)
