96802c95d544c69660c22e4c519e374cbb791dcc
tttthomasssss <tttthomasssss@gmail.com>
2016-05-11 13:08:23 +0100

    more echo

--- tools/batch_intrinsic_word_similarity_evaluation.sh
+++ tools/batch_intrinsic_word_similarity_evaluation.sh
@@ -20,6 +20,8 @@ if [ -z $DATA_HOME ]; then
 	DATA_HOME="~/.wort_data"
 fi
 
+echo "Evaluating wort models on $DATASETS..."
+
 find $INPUT_PATH -name "*$PATTERN*" | while read line; do
 	INPUT_FILE=$(basename $line)
 	echo "Running intrinsic evaluation with model=$INPUT_FILE..."
