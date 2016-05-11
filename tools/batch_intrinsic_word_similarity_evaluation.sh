09d9088f5e0bdeec1ebd91b87171bd4c893ba896
tttthomasssss <tttthomasssss@gmail.com>
2016-05-11 11:12:10 +0100

    stuff

--- tools/batch_intrinsic_word_similarity_evaluation.sh
+++ tools/batch_intrinsic_word_similarity_evaluation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Parse options
-while getopts ":i:h:p:" OPT; do
+while getopts ":d:i:h:p:" OPT; do
 	case $OPT in
+		d ) DATASETS=($OPTARG) ;;
+
 		i ) INPUT_PATH=($OPTARG) ;;
 
 		h ) DATA_HOME=($OPTARG) ;;
@@ -18,6 +20,6 @@ fi
 find $INPUT_PATH -name "*$PATTERN*" | while read line; do
 	INPUT_FILE=$(basename $line)
 	echo "Running intrinsic evaluation with model=$INPUT_FILE..."
-	python -m wort.evaluation -ip $INPUT_PATH -i $INPUT_FILE -dh $DATA_HOME
+	python -m wort.evaluation -ip $INPUT_PATH -i $INPUT_FILE -dh $DATA_HOME -ds $DATASETS
 	echo "==============================================================================="
 done
