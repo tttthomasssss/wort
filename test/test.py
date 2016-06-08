94d2953332f7093db4a368a5b88a2fed8e6aebc5
tttthomasssss <tttthomasssss@gmail.com>
2016-06-08 16:14:07 +0100

    no 0.75, but 4

--- test/test.py
+++ test/test.py
@@ -446,8 +446,8 @@ def vectorize_bnc():
 	import math
 	for log_sppmi, sppmi in zip([0, math.log(5), math.log(10)], [0, 5, 10]):
 		for pmi_type in ['ppmi']:
-			for cds in [1., 0.75]:
-				for window_size in [1, 2, 3, 5]:
+			for cds in [1.]:
+				for window_size in [1, 2, 3, 4, 5]:
 					for weighting_fn in ['constant', 'aggressive', 'very_aggressive', 'harmonic', 'distance', 'inverse_harmonic', 'gaussian']:
 						print('CONFIG: pmi_type={}; window_size={}; cds={}; shift={}; context_weighting={}...'.format(pmi_type, window_size, cds, sppmi, weighting_fn))
 						transformed_out_path = os.path.join(paths.get_dataset_path(), 'bnc', 'coling_wort', 'wort_model_ppmi_lemma-True_window-{}_cds-{}-sppmi_shift-{}_{}'.format(
