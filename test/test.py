c0d7094e3d9cdbc4cd49dfa661b5bf083789b6d1
tttthomasssss <tttthomasssss@gmail.com>
2016-06-14 11:08:16 +0100

    order 7

--- test/test.py
+++ test/test.py
@@ -447,7 +447,7 @@ def vectorize_bnc():
 	for log_sppmi, sppmi in zip([0, math.log(5), math.log(10)], [0, 5, 10]):
 		for pmi_type in ['ppmi']:
 			for cds in [1.]:
-				for window_size in [3, 5, 10]:
+				for window_size in [3, 5, 10, 7]:
 					for weighting_fn in ['constant', 'aggressive', 'very_aggressive', 'harmonic', 'distance', 'inverse_harmonic', 'gaussian']:
 						print('CONFIG: pmi_type={}; window_size={}; cds={}; shift={}; context_weighting={}...'.format(pmi_type, window_size, cds, sppmi, weighting_fn))
 						transformed_out_path = os.path.join(paths.get_dataset_path(), 'bnc', 'coling_wort', 'wort_model_ppmi_lemma-True_window-{}_cds-{}-sppmi_shift-{}_{}'.format(
