62d5edb7e69d9a80087106df58e09d7bc8866451
tttthomasssss <th0mas.ko6er@gmail.com>
2016-06-15 11:32:14 +0100

    bnc, only order 7

--- test/test.py
+++ test/test.py
@@ -444,10 +444,10 @@ def vectorize_bnc():
 
 	print('Word whitelist contains {} words!'.format(len(whitelist)))
 	import math
-	for log_sppmi, sppmi in zip([0, math.log(5), math.log(10)], [0, 5, 10]):
+	for log_sppmi, sppmi in zip([0], [0]):#zip([0, math.log(5), math.log(10)], [0, 5, 10]):
 		for pmi_type in ['ppmi']:
 			for cds in [1.]:
-				for window_size in [3, 5, 10, 7]:
+				for window_size in [7]:#[3, 5, 10, 7]:
 					for weighting_fn in ['constant', 'aggressive', 'very_aggressive', 'harmonic', 'distance', 'inverse_harmonic', 'gaussian']:
 						print('CONFIG: pmi_type={}; window_size={}; cds={}; shift={}; context_weighting={}...'.format(pmi_type, window_size, cds, sppmi, weighting_fn))
 						transformed_out_path = os.path.join(paths.get_dataset_path(), 'bnc', 'coling_wort', 'wort_model_ppmi_lemma-True_window-{}_cds-{}-sppmi_shift-{}_{}'.format(
