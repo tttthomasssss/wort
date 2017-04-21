8308c3d38dcbf7e2e553a2d0616356b9bad61b42
tttthomasssss <th0mas.ko6er@gmail.com>
2017-04-21 14:33:50 +0100

    fixed bug that unneccesarily converted the dense SVDed word vectors into a sparse matrix

--- wort/core/dim_reduction.py
+++ wort/core/dim_reduction.py
@@ -12,13 +12,13 @@ def svd_dim_reduction(X, **kwargs):
 	eig_weighting = kwargs.pop('eig_weighting', 1.)
 
 	logging.info('Applying SVD with dimensionality={}...'.format(dim))
-	Ut, S, Vt = sparsesvd(X.tocsc() if sparse.issparse(X) else sparse.csc_matrix(X), dim)
+	Ut, s, Vt = sparsesvd(X.tocsc() if sparse.issparse(X) else sparse.csc_matrix(X), dim)
 
 	# Perform Context Weighting
-	S = sparse.csr_matrix(np.diag(S ** eig_weighting))
+	S = np.diag(s ** eig_weighting)
 
-	W = sparse.csr_matrix(Ut.T).dot(S) # Word vectors
-	C = sparse.csr_matrix(Vt.T).dot(S) # Context vectors
+	W = Ut.T.dot(S) # Word vectors
+	C = Vt.T.dot(S) # Context vectors
 
 	return W, C
 
