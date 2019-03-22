fcaaf6034fe3bb495a4e9a8f8bed8af16c4a0b66
tttthomasssss <th0mas.ko6er@gmail.com>
2019-03-22 10:39:46 +0000

    added option to skip top n dimensions when performing dimensionality reduction

--- wort/core/dim_reduction.py
+++ wort/core/dim_reduction.py
@@ -7,9 +7,10 @@ from sparsesvd import sparsesvd
 import numpy as np
 
 
-def svd_dim_reduction(X, **kwargs): # TODO: Support discarding top m contexts (see Sahlgren & Lenci 2016, and my thesis)
+def svd_dim_reduction(X, **kwargs):
 	dim = kwargs['dimensionality'] # Fail loudly if not provided
 	eig_weighting = kwargs.pop('eig_weighting', 1.)
+	skip_top_n_dimensions = kwargs.pop('skip_top_n_dimensions', 0)
 
 	logging.info('Applying SVD with dimensionality={}...'.format(dim))
 	Ut, s, Vt = sparsesvd(X.tocsc() if sparse.issparse(X) else sparse.csc_matrix(X), dim)
@@ -17,20 +18,24 @@ def svd_dim_reduction(X, **kwargs): # TODO: Support discarding top m contexts (s
 	# Perform Context Weighting
 	S = np.diag(s ** eig_weighting)
 
-	W = Ut.T.dot(S) # Word vectors
-	C = Vt.T.dot(S) # Context vectors
+	W = Ut.T.dot(S)[:, skip_top_n_dimensions:] # Word vectors
+	C = Vt.T.dot(S)[:, skip_top_n_dimensions:] # Context vectors
 
 	return W, C
 
 
 def nmf_dim_reduction(X, **kwargs):
 	dim = kwargs.pop('dimensionality') if 'dimensionality' in kwargs else kwargs.pop('n_components') # must be one of the two, latter is sklearn NMF lingo
+	skip_top_n_dimensions = kwargs.pop('skip_top_n_dimensions', 0)
 
 	logging.info('Applying NMF with dimensionality={}...'.format(dim))
 	nmf = NMF(n_components=dim, **kwargs)
 
 	W = nmf.fit_transform(X)
 	C = nmf.components_
+
+	W = W[:, skip_top_n_dimensions:]
+	C = C[:, skip_top_n_dimensions:]
 
 	return W, C
 
